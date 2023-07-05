c98fdd38524fb34981d2cbf11f01966bb36babf5
Shubham Dubey <shubhamshd99@gmail.com>
2023-07-06 01:14:40 +0530

    generic ts typing challenge

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,27 +1,51 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
-]
-
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
-
-    const newTodo = {
-        id,
-        title: todo,
-        status: "todo",
-    }
-
-    todoItems.push(newTodo)
-
-    return newTodo
+interface TDItems {
+  id: number;
+  title: string;
+  status: TDItemStatus;
+  completedOn?: Date;
 }
 
-function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
+enum TDItemStatus {
+  Done = "done",
+  InProgress = "in-progress",
+  ToDo = "todo",
 }
 
-const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+const todoItems: TDItems[] = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    status: TDItemStatus.Done,
+    completedOn: new Date("2021-09-11"),
+  },
+  { id: 2, title: "Learn TypeScript", status: TDItemStatus.InProgress },
+  {
+    id: 3,
+    title: "Write the best app in the world",
+    status: TDItemStatus.ToDo,
+  },
+];
 
-console.log(JSON.stringify(newTodo))
+function addTodoItem(todo: string): TDItems {
+  const id = getNextId(todoItems);
+
+  const newTodo: TDItems = {
+    id,
+    title: todo,
+    status: TDItemStatus.ToDo,
+  };
+
+  todoItems.push(newTodo);
+
+  return newTodo;
+}
+
+function getNextId<T extends { id: number }>(items: T[]): number {
+  return items.reduce((max, x) => (x.id > max ? x.id : max), 0) + 1;
+}
+
+const newTodo = addTodoItem(
+  "Buy lots of stuff with all the money we make from the app"
+);
+
+console.log(JSON.stringify(newTodo));
